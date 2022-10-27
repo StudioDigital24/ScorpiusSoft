fd716e59b27f3ae0f5202fde3d7af7c38d35ca0a
Luisa Maria Gil Garcia <luisamariagilgarcia24@gmail.com>
2022-10-26 20:52:29 -0500

    Sprint 2

---
+++ src/main/java/com/studiodigital24/scorpiusSoft/ScorpiusSoftApplication.java
@@ -0,0 +1,13 @@
+package com.studiodigital24.scorpiusSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScorpiusSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScorpiusSoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studiodigital24/scorpiusSoft/ScorpiusSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studiodigital24.scorpiusSoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScorpiusSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
